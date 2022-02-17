838abe32aaca88c366a891e644ed42b2f39480ef
yahya-ux <yahayasalim2022@gmail.com>
2022-02-17 11:59:19 +0300

    use Type casting

---
+++ bin/Type_casting_dart.dart
@@ -0,0 +1,21 @@
+void main()
+{
+  //Type casting in dart
+  //Type one
+  var num =10;
+  var x =num.toString();
+  print("Welcome "+x);
+  
+  var p =false;
+  var b=p.toString();
+  print("Welcome "+b);
+
+  //Type two
+  var l = "55";
+  var g = int.parse(l)+9;
+  print ("The number is = $g");
+
+
+
+
+}
